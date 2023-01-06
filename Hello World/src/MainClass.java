37e6a3159d1fa6a881cf33641fad07da6764ecd3
jayesha-s <jayesha3430@gmail.com>
2023-01-06 20:26:00 +0800

    first message

---
+++ Hello World/src/MainClass.java
@@ -0,0 +1,12 @@
+public class MainClass {
+    public static void main(String[] args) {
+
+        int a = 3;
+
+        System.out.println("Hello, World!");
+
+        int arr [] = new int[2];
+        
+    }
+
+}
